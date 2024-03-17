b3f13e9c485884c30a09933449a8030e3d31f1be
Rishabh Gundavarapu <rishabh040801@gmail.com>
2024-03-16 18:30:24 -0700

    first changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,9 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Rishabh.</span> I'm a{" "}
-        <span className="font-bold">recent data science graduate</span> with{" "}
-        <span className="font-bold">a focus on data analytics and machine learning.
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
