995156c6a2d944ab20746aca2448c700cae9d04d
Rishabh Gundavarapu <rishabh040801@gmail.com>
2024-04-22 15:00:47 -0700

    changed about page

--- components/about.tsx
+++ components/about.tsx
@@ -20,12 +20,8 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Data Science from UW-Madison.</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
+        <span className="font-medium">Data Science from UW-Madison.</span>
+         My core stack
         is{" "}
         <span className="font-medium">
           Python, R, SQL, Tableau for Data Analysis and sklearn, Tensorflow, PyTorch for Machine Learning.
@@ -37,11 +33,8 @@ export default function About() {
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        video games, playing badminton, and watching F1. I also love{" "}
+        <span className="font-medium">chocolate</span> and can finish any pint-sized chocolate ice cream in a single sitting.
       </p>
     </motion.section>
   );
